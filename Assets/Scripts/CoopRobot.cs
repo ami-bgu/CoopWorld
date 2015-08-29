d56601d70070c946dcf72f11411079006b7681c5
Itai Goren <grimil@gmail.com>
2015-08-29 18:05:49 +0300

    Added  Particle System for spawning added CoopRobot script

---
+++ Assets/Scripts/CoopRobot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoopRobot : MonoBehaviour {
+
+    public GameObject SpawnEffect;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(SpawnEffect,this.transform.position,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
